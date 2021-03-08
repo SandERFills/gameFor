0568c92cff51915cbe838e961ea6ff89eafa16b7
SandERFills <salagaev98@mail.ru>
2021-03-08 21:50:27 +0300

    begin com

---
+++ Actor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace behaviorOne
+{
+    public class Actor
+    {
+        private string heroName;
+        private int healhPoints;
+        private int damage;
+        public Actor(){}
+        public Actor(string name,int hp,int dmg){
+                this.heroName = name;
+                this.healhPoints=hp;
+                this.damage=dmg;
+Console.WriteLine(heroName);
+        }
+        public void PrintName(){
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using behaviorOne;
+
+namespace ConsoleWar
+{
+    class Program
+    {
+        // public bool AttackeIter(Actor attacer,Actor defender)
+        // {
+        //    Console.WriteLine($"{attacer} бьёт {defender}");
+
+        // }
+
+        static void Main(string[] args)
+        {
+            Actor hero =new Actor("Dirty",100,20);
+           
+        }
+    }
+}
